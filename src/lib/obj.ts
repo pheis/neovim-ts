153f3a668914273140dd07a21d0020c0ed6959fe
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2022-12-10 18:04:33 +0200

    fix: wrap fns to arrow fns to remove this

--- src/lib/obj.ts
+++ src/lib/obj.ts
@@ -1,11 +1,15 @@
 type ObjectKey = string | symbol | number
 
-const keys: <Obj extends Record<string, unknown>>(obj: Readonly<Obj>) => (keyof Obj)[] = Object.keys
+const keys: <Obj extends Record<string, unknown>>(
+  obj: Readonly<Obj>
+) => (keyof Obj)[] = obj => Object.keys(obj)
 
 type Entry<Obj extends Record<ObjectKey, any>> = {
   [Key in keyof Obj]: [Key, Obj[Key]]
 }[keyof Obj]
 
-const entries: <Obj extends Record<string, any>>(obj: Obj) => Entry<Obj>[] = Object.entries
+const entries: <Obj extends Record<string, any>>(
+  obj: Obj
+) => Entry<Obj>[] = obj => Object.entries(obj)
 
-export {keys, entries}
+export { keys, entries }
