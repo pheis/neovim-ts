c86a39187cd7329f2810842086847bb7a611c5ea
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2022-12-10 17:48:48 +0200

    feat: keymapping

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,36 @@
-import { partial } from "./lib/fn"
+import { lazy, partial } from "./lib/fn"
+import { chars } from "./lib/string"
+
 vim.cmd("colo zellner")
 
-const imap = partial(vim.keymap.set, "i")
+const set = vim.keymap.set
+const nset = partial(set, "n")
+const vset = partial(set, "v")
 
-imap("fd", "<Esc>")
+const cmd: (cmd: string) => () => void = vimCmd => lazy(vim.cmd, vimCmd)
 
-// vim.keymap.set("i", "fd", "<Esc>")
+// fd to Esc
+set(chars("ivt"), "fd", "<Esc>")
 
-// const n
+// Easier Moving between splits
+chars("HJKL").forEach(key => nset(`C-${key}`, `:<C-W><C-${key}>`))
+
+// eslint-disable-next-line no-useless-escape
+// vim script version was...
+// vnoremap // y/\V<C-r>=escape(@",'/\')<CR><CR>
+vset("//", "y/\\V<C-r>=escape(@\",'/')<CR><CR>")
+
+// very magic
+set(chars("vn"), "/", "/\v)")
+
+// prettier-ignore
+const nmaps = {
+  // "" space space to like spacemacs
+  "<leader><leader>": ":",
+  "yon": (): void => ["set invnumber", "set invrelativenumber"].forEach(vim.cmd),
+  "yoh": cmd("set invhlsearch"),
+  "]q": cmd("cnext"),
+  "[q": cmd("cprev"),
+  "<leader>m": cmd("make"),
+  "<leader>w": cmd("write"),
+}
