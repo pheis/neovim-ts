f00f97975237404a9b160c46b53e1fd0928ef0c1
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2022-12-10 17:10:33 +0200

    feat: partial helper fn

---
+++ src/lib/fn.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type FnLike = (...args: any[]) => any
+
+type Tail<Arr extends any[]> = Arr extends [any, ...infer Rest] ? Rest : never
+
+type Take<
+  N extends number,
+  Arr extends any[],
+  Taken extends any[] = []
+> = Taken["length"] extends N
+  ? Taken
+  : Take<N, Arr, [...Taken, Arr[Taken["length"]]]>
+
+type Drop<
+  N extends number,
+  Arr extends any[],
+  Dropped extends any[] = []
+> = Dropped["length"] extends N ? Arr : Drop<N, Tail<Arr>, [...Dropped, Arr[0]]>
+
+export const partial =
+  <Fn extends FnLike, Params extends readonly unknown[]>(
+    fn: Fn,
+    ...params: Params extends Take<Params["length"], Parameters<Fn>>
+      ? Params
+      : never
+  ) =>
+  (...rest: Drop<Params["length"], Parameters<Fn>>): ReturnType<Fn> =>
+    fn(...(params.concat(rest) as never[])) as never
