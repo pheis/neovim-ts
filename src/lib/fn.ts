0cdcccf9844ca005d1974d9344e1f671a1145967
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2022-12-10 19:25:07 +0200

    feat: some broken pipe types

--- src/lib/fn.ts
+++ src/lib/fn.ts
@@ -3,6 +3,35 @@ type FnLike = (...args: any[]) => any
 
 type Tail<Arr extends any[]> = Arr extends [any, ...infer Rest] ? Rest : never
 
+type GetOutput<Value, Fns extends any[]> = Fns["length"] extends 0
+  ? Value
+  : Value extends Parameters<Fns[0]>[0]
+  ? GetOutput<ReturnType<Fns[0]>, Tail<Fns>>
+  : never
+
+type AllUnary<
+  Fns extends FnLike[],
+  Checked extends FnLike[] = []
+> = Checked["length"] extends Fns["length"]
+  ? true
+  : 1 extends Parameters<Fns[Checked["length"]]>["length"]
+  ? AllUnary<Fns, [...Checked, Fns[Checked["length"]]]>
+  : false
+
+export type PipeOutput<Tuple extends any[]> = Tuple extends [
+  infer First,
+  ...infer Fns extends FnLike[]
+]
+  ? AllUnary<Fns> extends true
+    ? GetOutput<First, Fns>
+    : never
+  : never
+
+export const pipe = <Params extends any[]>(
+  ...[first, ...rest]: Params
+): PipeOutput<Params> =>
+  rest.reduce((acc, fn: FnLike) => fn(acc) as never, first) as never
+
 type Take<
   N extends number,
   Arr extends any[],
