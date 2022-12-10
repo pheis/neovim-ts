79165c096316970ef1ba5bdfd486d086f62d58d4
Pyry Heiskanen <pyry.heiskanen@iki.fi>
2022-12-10 23:22:08 +0200

    feat: options

---
+++ src/options.ts
@@ -0,0 +1,35 @@
+const options = {
+  expandtab: true,
+  shiftwidth: 2,
+  tabstop: 2,
+  softtabstop: 2,
+
+  scrolloff: 3,
+  sidescrolloff: 3,
+
+  updatetime: 100,
+
+  relativenumber: true,
+
+  showcmd: true,
+
+  wildmenu: true,
+
+  showmatch: true,
+
+  incsearch: true,
+  hlsearch: true,
+
+  termguicolors: true,
+
+  winblend: 10,
+  pumblend: 10,
+  signcolumn: "yes",
+}
+
+const setOptions = (): void =>
+  Object.entries(options).forEach(
+    ([key, value]) => (vim.opt[key as never] = value as never)
+  )
+
+export { setOptions }
